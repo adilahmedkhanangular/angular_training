410aedfa01e0ee853354c76a9f3f47a8c183f5ad
adilahmedkhanangular <67585402+adilahmedkhanangular@users.noreply.github.com>
2020-07-01 16:32:11 +0500

    2

--- src/app/manual/manual.component.ts
+++ src/app/manual/manual.component.ts
@@ -5,10 +5,6 @@ import { Component } from "@angular/core"
     template: `<h3>Manual Component</h3><br/>
     <form><div class="form-group"><label for="exampleInputEmail1">{{lblEmailAddress}}</label><input class='form-control' type='email' id='exampleInputEmail1' [placeholder]='lblEmailAddress' /></div>
     <div class="form-group"><label for="exampleInputPassword1">{{lblPassword}}</label><input class='form-control' type='password' id='exampleInputPassword1' [placeholder]='lblPassword' /></div>
-    <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
     <button type="button" (click)="printLogin()" class="btn btn-primary">Print</button><br /><label>{{Message}}</label></form>`
 })
 
